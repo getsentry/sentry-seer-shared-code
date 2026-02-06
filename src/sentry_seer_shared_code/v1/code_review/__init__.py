19fdeed5622d8264921a2eb44dd8ba47644c3a40
Armen Zambrano G. <44410+armenzg@users.noreply.github.com>
2026-02-06 11:09:53 -0500

    Add lost files

---
+++ src/sentry_seer_shared_code/v1/code_review/__init__.py
@@ -0,0 +1,31 @@
+"""
+Pydantic v1 code review models for Sentry-Seer integration.
+"""
+
+from sentry_seer_shared_code.v1.code_review.models import (
+    BugPredictionSpecificInformation,
+    GitProvider,
+    SeerCodeReviewConfig,
+    SeerCodeReviewFeature,
+    SeerCodeReviewRequestForPrClosed,
+    SeerCodeReviewRequestForPrReview,
+    SeerCodeReviewRequestType,
+    SeerCodeReviewTaskRequestForPrClosed,
+    SeerCodeReviewTaskRequestForPrReview,
+    SeerCodeReviewTrigger,
+)
+from sentry_seer_shared_code.v1.seer.models import SeerRepoDefinition
+
+__all__ = [
+    "BugPredictionSpecificInformation",
+    "GitProvider",
+    "SeerCodeReviewConfig",
+    "SeerCodeReviewFeature",
+    "SeerCodeReviewRequestForPrClosed",
+    "SeerCodeReviewRequestForPrReview",
+    "SeerCodeReviewRequestType",
+    "SeerCodeReviewTaskRequestForPrClosed",
+    "SeerCodeReviewTaskRequestForPrReview",
+    "SeerCodeReviewTrigger",
+    "SeerRepoDefinition",
+]
